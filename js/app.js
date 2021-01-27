f3140f10ba08a3bc397e8d7e13c319a30151665d
epgfm <recon@vlay.org>
2021-01-27 11:14:26 +0100

    Changed code so that the green border is dynamically added as the mouse hovers a paragraph

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-function handleClickP(e) {
+function handleMouseoverP(e) {
     var p = e.target;
     p.classList.add("gbox");
 }
 
+function handleMouseoutP(e) {
+    var p = e.target;
+    p.classList.remove("gbox");
+}
 
 function init() {
     // This code will be executed when page is completely loaded.
     console.log("Page loaded!");
     var ps = document.getElementsByTagName("p");
     for (var i = 0; i < ps.length; i++) {
-        ps[i].addEventListener("click", handleClickP);
+        ps[i].addEventListener("mouseover", handleMouseoverP);
+        ps[i].addEventListener("mouseout", handleMouseoutP);
     }
 }
 
