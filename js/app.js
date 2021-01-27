8da8b6462edc18ed77cd8fc6e6f51fa265fa2abb
epgfm <recon@vlay.org>
2021-01-27 10:10:33 +0100

    Added p2 click handler

--- js/app.js
+++ js/app.js
@@ -4,11 +4,19 @@ function handleClickPa() {
     pa.classList.add("gbox");
 }
 
+function handleClickPb() {
+    console.log("b clicked!");
+    var pb = document.getElementById("b");
+    pb.classList.add("gbox");
+}
+
 function init() {
     // This code will be executed when page is completely loaded.
     console.log("Page loaded!");
     var pa = document.getElementById("a");
     pa.addEventListener("click", handleClickPa);
+    var pb = document.getElementById("b");
+    pb.addEventListener("click", handleClickPb);
 }
 
 document.addEventListener("DOMContentLoaded", init);
